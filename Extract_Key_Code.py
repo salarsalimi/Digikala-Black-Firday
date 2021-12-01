b3d9e8e9d7c4f01f8c73ab525eca39b0907fc98a
salarsalimi <33908999+salarsalimi@users.noreply.github.com>
2021-12-01 17:26:59 +0330

    Update Extract_Key_Code.py

--- Extract_Key_Code.py
+++ Extract_Key_Code.py
@@ -7,54 +7,50 @@ import numpy as np
 from os.path import exists
 
 
-for j in range(0,46):
+for j in range(0,46):                                                                                # Loop through pages 1 to 47
     pagenum = j + 1
-    print(pagenum)
-    url = "https://www.digikala.com/treasure-hunt/products/?pageno=" + str(pagenum) + "&sortby=4"
-    page = requests.get(url).text
+    url = "https://www.digikala.com/treasure-hunt/products/?pageno=" + str(pagenum) + "&sortby=4"    # Link to Competition page products
+    page = requests.get(url).text                                                                    # Extract html source code
     soup = BeautifulSoup(page,'lxml')
-    jobs = soup.find_all('a', class_ = 'c-product-box__img' )
-    jobs_str = str(jobs)
-    pattern1 = re.compile(r'href=\"(\S*)\"')
-    x = pattern1.findall(jobs_str)
+    products = soup.find_all('a', class_ = 'c-product-box__img' )                                    # Get All products urls from their class 
+    products_str = str(products)                                                                      
+    pattern1 = re.compile(r'href=\"(\S*)\"')                                                         # Extract products url with regex
+    x = pattern1.findall(products_str)
 
-    for i in x:    
+    for i in x:                                                                                      # Loop Products 
         url2 = "https://www.digikala.com" + i
-        
-        page2 = requests.get(url2).text
+        page2 = requests.get(url2).text                                                              # Extract html source code of product page
         soup2 = BeautifulSoup(page2,'lxml')
-        
-        jobs2 = soup2.find_all('li', class_ = 'js-product-thumb-img' )
-        jobs_str2 = str(jobs2)
-        pattern2 = re.compile(r'data-src=\"(\S*\.jpg)')
-        x2 = pattern2.findall(jobs_str2)
+        images = soup2.find_all('li', class_ = 'js-product-thumb-img' )                              # Get All Images urls from their class               
+        images_str = str(images)                
+        pattern2 = re.compile(r'data-src=\"(\S*\.jpg)')                                              # Extract images url with regex
+        x2 = pattern2.findall(images_str)
         
 
 
-        for k in x2:
-            newstr1 = k.replace("/", "")
-            newstr = newstr1.replace(":", "")
-                
-            urllib.request.urlretrieve(k, 'C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+newstr)
+        for k in x2:                                                                                 # Iterate through images and correct their name in order to save them
+            imageurl = k.replace("/", "")
+            imagefile = imageurl.replace(":", "")   
+            urllib.request.urlretrieve(k, 'C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+imagefile)   # Downlaod Image
             try:
-                original = cv2.imread('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+newstr)
-                duplicate = cv2.imread('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\'+newstr)
+                original = cv2.imread('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+imagefile)
+                duplicate = cv2.imread('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\'+imagefile)
                 difference = cv2.subtract(original, duplicate)
                 b, g, r = cv2.split(difference)
-                if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+                if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:                   # Check to see if images are the same 
                     print("The images are completely Equal")
                 else :
                     print("Found It")
-                    print(newstr)
-                    print(url2)
+                    print(imagefile)
+                    print(imageurl)
                     f = open("key.txt", "a")
-                    f.write(newstr)
+                    f.write(imagefile)
                     f.close()
                     # Imports PIL module 
                     from PIL import Image
   
                     # open method used to open different extension image file
-                    im = Image.open('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+newstr) 
+                    im = Image.open('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+imagefile) 
   
                     # This method will show image in any image viewer 
                     im.show() 
@@ -63,10 +59,10 @@ for j in range(0,46):
                 from PIL import Image
   
                 # open method used to open different extension image file
-                im = Image.open('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+newstr) 
+                im = Image.open('C:\\Users\\Administrator.S-VM1\\Desktop\\python\\Hunt\\images\\'+imagefile) 
   
                 # This method will show image in any image viewer 
                 im.show() 
                 f = open("err.txt", "a")
-                f.write(newstr)
+                f.write(imagefile)
                 f.close()
